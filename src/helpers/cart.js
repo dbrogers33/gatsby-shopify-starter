179af196c870d4c48951bdb2fe62a1fd1faa40d6
dbrogers33 <denhamrogers10@gmail.com>
2020-07-17 15:47:53 -0500

    adding cart functions

---
+++ src/helpers/cart.js
@@ -0,0 +1,119 @@
+import React, { createContext, useReducer, useEffect, useContext } from "react"
+import Client from "shopify-buy"
+
+import { cartReducer } from "../reducers/cartReducer"
+
+const SHOPIFY_CHECKOUT_STORAGE_KEY = "shopify_checkout_id"
+
+const client = Client.buildClient({
+  storefrontAccessToken: process.env.SHOP_TOKEN,
+  domain: `${process.env.SHOP_NAME}.myshopify.com`,
+})
+
+function createNewCheckout(cart) {
+  return cart.checkout.create()
+}
+
+function fetchCheckout(client, id) {
+  return client.checkout.fetch(id)
+}
+
+const initialCartState = {
+  client,
+  isAdding: false,
+  checkout: { lineItems: [] },
+}
+
+const CartContext = createContext({
+  cart: initialCartState,
+  setCart: () => null,
+})
+
+const CartContextProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, initialCartState)
+
+  useEffect(() => {
+    const initializeCheckout = async () => {
+      const isBrowser = typeof window !== "undefined"
+      const existingCheckoutId = isBrowser
+        ? localStorage.getItem(SHOPIFY_CHECKOUT_STORAGE_KEY)
+        : null
+      if (existingCheckoutId) {
+        try {
+          const checkout = await fetchCheckout(client, existingCheckoutId)
+          if (!checkout.completedAt) {
+            console.log("Checkout fetched from Shopify with id", checkout.id)
+            console.log("Checkout contains", checkout.lineItems)
+            if (checkout) {
+              console.log("Updating checkout in browser")
+              dispatch({ type: "UPDATE_CHECKOUT", checkout })
+            }
+            return {
+              client,
+              checkout,
+              isAdding: false,
+            }
+          }
+        } catch {
+          console.log(
+            "Something went wrong and the checkout key had to be erased"
+          )
+          localStorage.setItem(SHOPIFY_CHECKOUT_STORAGE_KEY, null)
+        }
+      }
+      const newCheckout = await createNewCheckout(client)
+      localStorage.setItem(SHOPIFY_CHECKOUT_STORAGE_KEY, newCheckout.id)
+      console.log("The new checkout id stored is", newCheckout.dispatch)
+      return {
+        client,
+        newCheckout,
+        isAdding: false,
+      }
+    }
+
+    initializeCheckout()
+  }, [])
+
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+function useAddItemToCart() {
+  const { dispatch } = useContext(CartContext)
+
+  async function addItemToCart(variantId, quantity) {
+    if (variantId === "" || !quantity) {
+      console.error("Both a size and quantity are required.")
+      return
+    }
+
+    const isBrowser = typeof window !== "undefined"
+    const checkoutId = isBrowser
+      ? localStorage.getItem(SHOPIFY_CHECKOUT_STORAGE_KEY)
+      : null
+    const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }]
+
+    const newCheckout = await client.checkout.addLineItems(
+      checkoutId,
+      lineItemsToAdd
+    )
+
+    dispatch({ type: "UPDATE_CHECKOUT", checkout: newCheckout })
+  }
+
+  return addItemToCart
+}
+
+function useGoToCheckout() {
+  const { cart } = useContext(CartContext)
+
+  return () => {
+    console.log("Going to Checkout!")
+    window.open(cart.checkout.webUrl)
+  }
+}
+
+export { CartContext, CartContextProvider, useAddItemToCart, useGoToCheckout }
--- src/pages/templates/product.js
+++ src/pages/templates/product.js
@@ -2,6 +2,8 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import Hero from "../../components/hero/small-hero"
+import VariantItem from "../../components/VariantItem"
+
 
 const ProductTemplate = ({ pageContext, data }) => {
   const { product } = pageContext
@@ -12,21 +14,24 @@ const ProductTemplate = ({ pageContext, data }) => {
         <h1 className="text-3xl font-bold">{product.title}</h1>
         <h3 className="text-xl font-bold">${product.priceRange.minVariantPrice.amount}</h3>
         <div style={{ display: "flex", justifyContent: "space-around" }}>
-        {product.images.map(image => {
-          return (
-            <figure key={image.originalSrc}>
-              <img
-                src={image.originalSrc}
-                alt={product.title}
-                style={{
-                  height: "auto",
-                  width: "50%",
-                }}
-              />
-            </figure>
-          )
-        })}
-      </div>
+          {product.images.map(image => {
+            return (
+              <figure key={image.originalSrc}>
+                <img
+                  src={image.originalSrc}
+                  alt={product.title}
+                  style={{
+                    height: "auto",
+                    width: "50%",
+                  }}
+                />
+              </figure>
+            )
+          })}
+        </div>
+        {/* {product.variants.map(variant => {
+          return <VariantItem variant={variant} key={variant.shopifyId} />
+        })} */}
         <div>{product.description}</div>
       </div>
     </Layout>
